f5c15e659c05c45b0223f9be7190343aca5744da
vickynicchi <161743968+vickynicchi@users.noreply.github.com>
2024-07-21 12:13:40 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// You can add any necessary JavaScript here
+// For example, smooth scrolling for navigation:
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
