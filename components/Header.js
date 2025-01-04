c34233cadecd0614b84907f1e3a20afa95f6f2ab
antoniofrancaib <antoniofrancaib@gmail.com>
2025-01-04 16:59:26 +0100

    nw

--- components/Header.js
+++ components/Header.js
@@ -1,31 +1,61 @@
 // components/Header.js
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function Header() {
   return (
     <header className="site-header">
-      <div className="logo">V. Nicchi</div>
+      {/* We remove the logo/brand text (e.g., "V. Nicchi"), 
+          so just the nav remains. */}
+
       <nav className="top-nav">
         <ul>
+          {/* Home */}
           <li>
             <Link href="/">
               <a>Home</a>
             </Link>
           </li>
+
+          {/* About */}
           <li>
             <Link href="/about">
               <a>About</a>
             </Link>
           </li>
+
+          {/* CV: points to /pdf/cv.pdf */}
           <li>
-            <Link href="/obras">
-              <a>Obras</a>
-            </Link>
+            <a
+              href="/pdf/cv.pdf"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              CV
+            </a>
           </li>
+
+          {/* Contact */}
           <li>
             <Link href="/contact">
               <a>Contact</a>
             </Link>
+          </li>
+
+          {/* Instagram icon/button */}
+          <li>
+            <a
+              href="https://www.instagram.com/__vickynicchi"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Image
+                src="/images/instagram-icon.png"
+                alt="Instagram"
+                width={20}
+                height={20}
+              />
+            </a>
           </li>
         </ul>
       </nav>
