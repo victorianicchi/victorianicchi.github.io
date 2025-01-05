78520ea4a9b48a8a04208352968b736b0bd17cbb
antoniofrancaib <antoniofrancaib@gmail.com>
2025-01-05 11:22:11 +0100

    nw

--- components/Header.js
+++ components/Header.js
@@ -8,23 +8,19 @@ export default function Header() {
 
   return (
     <header className="site-header">
+      {/* Main navigation */}
       <nav className="top-nav">
         <ul>
-          {/* Home */}
           <li>
             <Link href="/">
               <a>{lang === 'en' ? 'Home' : 'Inicio'}</a>
             </Link>
           </li>
-
-          {/* About */}
           <li>
             <Link href="/about">
               <a>{lang === 'en' ? 'About' : 'Acerca'}</a>
             </Link>
           </li>
-
-          {/* CV */}
           <li>
             <a
               href="/pdf/cv.pdf"
@@ -34,14 +30,32 @@ export default function Header() {
               {lang === 'en' ? 'CV' : 'CV'}
             </a>
           </li>
-
-          {/* Contact */}
           <li>
             <Link href="/contact">
               <a>{lang === 'en' ? 'Contact' : 'Contacto'}</a>
             </Link>
           </li>
+        </ul>
+      </nav>
 
+      {/* Social links + Language toggle */}
+      <div className="header-right">
+        <ul className="social-links">
+          {/* TikTok */}
+          <li>
+            <a
+              href="https://www.tiktok.com/@YourTikTokHandle"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Image
+                src="/images/tiktok-icon.png"
+                alt="TikTok"
+                width={20}
+                height={20}
+              />
+            </a>
+          </li>
           {/* Instagram */}
           <li>
             <a
@@ -57,16 +71,43 @@ export default function Header() {
               />
             </a>
           </li>
+          {/* LinkedIn */}
+          <li>
+            <a
+              href="https://www.linkedin.com/in/YourLinkedIn/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Image
+                src="/images/linkedin-icon.png"
+                alt="LinkedIn"
+                width={20}
+                height={20}
+              />
+            </a>
+          </li>
+          {/* X (Twitter) */}
+          <li>
+            <a
+              href="https://twitter.com/YourHandle"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Image
+                src="/images/x-icon.png"
+                alt="X"
+                width={20}
+                height={20}
+              />
+            </a>
+          </li>
         </ul>
-      </nav>
 
-      {/* Language Toggle Button -> "en" / "es" */}
-      <button
-        onClick={toggleLang}
-        className="lang-toggle-button"
-      >
-        {lang === 'en' ? 'es' : 'en'}
-      </button>
+        {/* Language toggle */}
+        <button onClick={toggleLang} className="lang-toggle-button">
+          {lang === 'en' ? 'es' : 'en'}
+        </button>
+      </div>
     </header>
   );
 }
